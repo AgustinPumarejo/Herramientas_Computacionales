aeca95a841611b43358a4ac637e5c112a03399be
Daniel Buendia <danielbuendia@MacBook-Air-de-Daniel-5.local>
2021-03-19 10:33:06 -0600

    Added python files

--- Python/cannon.py
+++ Python/cannon.py
@@ -1,65 +1,88 @@
-from turtle import *
+"""Cannon, hitting targets with projectiles
+
+Exercises
+
+1. Keep score by counting target hits.
+2. Vary the effect of gravity.
+3. Apply gravity to the targets.
+4. Change the speed of the ball.
+
+"""
+
 from random import randrange
-from freegames import square, vector
+from turtle import *
+from freegames import vector
 
-food = vector(0, 0)
-snake = [vector(10, 0)]
-aim = vector(0, -10)
+ball = vector(-200, -200)
+speed = vector(0, 0)
+targets = []
 
-def change(x, y):
-    "Change snake direction."
-    aim.x = x
-    aim.y = y
+def tap(x, y):
+    "Respond to screen tap."
+    if not inside(ball):
+        ball.x = -199
+        ball.y = -199
+        speed.x = (x + 200) / 25
+        speed.y = (y + 200) / 25
 
-def inside(head):
-    "Return True if head inside boundaries."
-    #return -200 < head.x < 190 and -200 < head.y < 190
-    return False
+def inside(xy):
+    "Return True if xy within screen."
+    return -200 < xy.x < 200 and -200 < xy.y < 200
 
-def move():
-    "Move snake forward one segment."
-    global food
-    head = snake[-1].copy()
-    head.move(aim)
-
-    if not inside(head) or head in snake:
-        square(head.x, head.y, 9, 'red')
-        update()
-        #return
-
-    snake.append(head)
-
-    food_check = vector(0,0)
-
-    if head == food:
-        print('Snake:', len(snake))
-        while food_check in snake:
-            x1=randrange(-15, 15) * 10
-            y1= randrange(-15, 15) * 10
-            food_check = vector(x1,y1)
-        food=food_check
-       
-       
-    #else:
-        #snake.pop(0)
-
-
+def draw():
+    "Draw ball and targets."
     clear()
 
-    for body in snake:
-        square(body.x, body.y, 9, 'black')
+    for target in targets:
+        goto(target.x, target.y)
+        dot(20, 'blue')
 
-    square(food.x, food.y, 9, 'green')
+    if inside(ball):
+        goto(ball.x, ball.y)
+        dot(6, 'red')
+
     update()
-    ontimer(move, 100)
+
+def move():
+    "Move ball and targets."
+    # Generate a new target at random times
+    if randrange(40) == 0:
+        y = randrange(-150, 150)
+        target = vector(200, y)
+        targets.append(target)
+
+    # Move the existing targets
+    for target in targets:
+        target.x -= 0.5
+
+    # Move the cannon shot
+    if inside(ball):
+        speed.y -= 0.35
+        ball.move(speed)
+
+    # Make a copy of the existing target list before redrawing
+    dupe = targets.copy()
+    targets.clear()
+
+    # Detect if the bullet hits a target
+    for target in dupe:
+        if abs(target - ball) > 13:
+            targets.append(target)
+
+    draw()
+
+    # Detect when a target reaches the left side
+    for target in targets:
+        if not inside(target):
+            #targets.remove(target)
+            return
+
+    ontimer(move, 50)
 
 setup(420, 420, 370, 0)
 hideturtle()
+up()
 tracer(False)
-listen()
-onkey(lambda: change(10, 0), 'Right')
-onkey(lambda: change(-10, 0), 'Left')
-onkey(lambda: change(0, 10), 'Up')
-onkey(lambda: change(0, -10), 'Down')
+onscreenclick(tap)
 move()
 done()
